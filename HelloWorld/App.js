0d261a0f00e4a776a56ed7d353dd1a52c743766d
Sridhar Ganesan <sridhar.ganesan@booking.com>
2018-07-05 23:31:44 +0200

    Working sampling - upload yet to be done

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Magnetometer, Accelerometer, Gyroscope } from "react-native-sensors";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Picker} from 'react-native';
 import KeepAwake from 'react-native-keep-awake';
 
 const Value = ({ name, value }) => (
@@ -10,41 +10,127 @@ const Value = ({ name, value }) => (
   </View>
 );
 
+acc_data = [];
+acc_t = 0;
+gyr_data = [];
+gyr_t = 0;
+mag_data = [];
+mag_t = 0;
+total_samples = 1; 
+sample_length_in_msec = 5000;
+interval_in_msec = 20;
+datapoints = sample_length_in_msec / interval_in_msec;
+start_t = 0;
+
 export default class App extends React.Component {
+  
+   log_data(x, y, z, t, type) {
+      if (type == 'M') {
+        if (mag_t < t && mag_data.length < datapoints) {
+            mag_data.push([x, y, z]);
+            mag_t = t;
+        }
+      } else if (type == 'G') {
+        if (gyr_t < t && gyr_data.length < datapoints) {
+            gyr_data.push([x, y, z]);
+            gyr_t = t;
+        }
+      } else if (type == 'A') {
+         if (acc_t < t && acc_data.length < datapoints) {
+            acc_data.push([x, y, z]);
+            acc_t = t;
+         }
+      }
+      if (mag_data.length == datapoints && gyr_data.length == datapoints && acc_data.length == datapoints) {
+          if (this.state.started) {
+             console.log("Uploading...");
+             this.samples++;
+             if (this.samples >= total_samples) {
+                this.toggle_and_reset();
+             }
+          }
+          mag_data = [];
+          gyr_data = [];
+          acc_data = [];
+      }
+   }
+
    constructor(props) {
     super(props);
+    this.samples = 0;
 
     new Accelerometer({
-      updateInterval: 400 // defaults to 100ms
+      updateInterval: interval_in_msec // defaults to 100ms
     })
-      .then(observable => {
-        observable.subscribe(({ x, y, z, timestamp }) => this.setState({ ax: x, ay: y, az: z, at: timestamp }));
-      })
+      .then(function(observable){
+        observable.subscribe(({ x, y, z, timestamp }) => this.log_data(x, y, z, timestamp, 'A'));
+      }.bind(this))
       .catch(error => {
         console.log("The sensor is not available");
       });
 
-    new Gyroscope({ updateInterval: 50
-    }).then(observable => {
-       observable.subscribe(({x, y, z, timestamp}) => this.setState({gx: x, gy: y, gz: z, gt: timestamp}));
-    }).catch(error => {
+    new Gyroscope({ updateInterval: interval_in_msec
+    }).then(function(observable) {
+       observable.subscribe(({x, y, z, timestamp}) => this.log_data(x, y, z, timestamp, 'G'));
+    }.bind(this)).catch(error => {
 	console.log("Gyroscope is not available");
     });
     
-    new Magnetometer({ updateInterval: 200})
-      .then(observable => { observable.subscribe(({x, y, z, timestamp}) => this.setState({mx: x, my: y, mz: z, mt: timestamp}));
-     }).catch(error => {
+    new Magnetometer({ updateInterval: interval_in_msec})
+      .then(function(observable)  { observable.subscribe(({x, y, z, timestamp}) => this.log_data(x, y, z, timestamp, 'M'));
+     }.bind(this)).catch(error => {
         console.log("Magnetometer is not available");
      });
-    this.state = { ax: 0, ay: 0, az: 0, at: 0, gx: 0, gy: 0, gz: 0, gt: 0, mx: 0, my: 0, mz: 0, mt: 0 };
+     this.state = {started: false, transport_mode: 0};
+  }
+  
+  toggle_and_reset() {
+     if (!this.state.started) {
+       console.log("Cleared....");
+       mag_data = [];
+       gyr_data = [];
+       acc_data = [];
+     }
+     started = !this.state.started;
+     this.setState({started: started});
+     if (started) {
+        KeepAwake.activate();
+     } else {
+        KeepAwake.deactivate();
+     }
   }
   
   componentDidMount() {
-    KeepAwake.activate();
+      KeepAwake.deactivate();
   }
 
   render() {
     return (
+       <View style={styles.container}>
+          <Value name="Runtime" value="5 minutes"/>
+          <Value name="Sample size" value="5 seconds"/>
+          <Value name="Sample interval" value="0.02 seconds"/>
+          <Picker
+            selectedValue={this.state.transport_mode}
+            style={{width: 200, height: 10}}
+            onValueChange={(itemValue, itemIndex) => this.setState({transport_mode: itemValue})}>
+            <Picker.Item label="Idle" value="0" />
+            <Picker.Item label="Walking" value="1" />
+            <Picker.Item label="Running" value="2" />
+            <Picker.Item label="Cycling" value="3" />
+            <Picker.Item label="Bike" value="4" />
+            <Picker.Item label="Auto" value="5" />
+            <Picker.Item label="Car" value="6" />
+            <Picker.Item label="Bus" value="7" />
+            <Picker.Item label="Tram" value="8" />
+            <Picker.Item label="Train" value="9" />
+            <Picker.Item label="Metro" value="10" />
+            <Picker.Item label="Flight" value="11" />
+            <Picker.Item label="Boat" value="12" />
+          </Picker>
+          <Button title={this.state.started? "Stop" : "Start"} onPress={() => this.toggle_and_reset()} />
+       </View>
+     /*
       <View style={styles.container}>
         <Text style={styles.headline}>Accelerometer values</Text>
         <Value name="ax" value={this.state.ax} />
@@ -62,7 +148,7 @@ export default class App extends React.Component {
         <Value name="mz" value={this.state.mz} />
         <Value name="mt" value={this.state.mt} />
       </View>
-      
+      */
     );
   } 
 }
@@ -71,11 +157,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'center', 
     justifyContent: 'center',
   },
   headline: {
-    fontSize: 30,
+    fontSize: 10,
     textAlign: "center",
     margin: 10
   },
@@ -85,11 +171,11 @@ const styles = StyleSheet.create({
   },
   valueValue: {
     width: 200,
-    fontSize: 20
+    fontSize: 10
   },
   valueName: {
     width: 50,
-    fontSize: 20,
+    fontSize: 10,
     fontWeight: "bold"
   },
   instructions: {
