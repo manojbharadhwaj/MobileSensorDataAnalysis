a1ba01523d0549fd7e6e23db0e877381faddae0d
Sridhar Ganesan <sridhar.ganesan@booking.com>
2018-07-05 13:45:40 +0200

    Adding magnetometer readings

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accelerometer, Gyroscope } from "react-native-sensors";
+import { Magnetometer, Accelerometer, Gyroscope } from "react-native-sensors";
 import { StyleSheet, Text, View } from 'react-native';
 import KeepAwake from 'react-native-keep-awake';
 
@@ -30,8 +30,13 @@ export default class App extends React.Component {
     }).catch(error => {
 	console.log("Gyroscope is not available");
     });
-
-    this.state = { ax: 0, ay: 0, az: 0, at: 0, gx: 0, gy: 0, gz: 0, gt: 0 };
+    
+    new Magnetometer({ updateInterval: 200})
+      .then(observable => { observable.subscribe(({x, y, z, timestamp}) => this.setState({mx: x, my: y, mz: z, mt: timestamp}));
+     }).catch(error => {
+        console.log("Magnetometer is not available");
+     });
+    this.state = { ax: 0, ay: 0, az: 0, at: 0, gx: 0, gy: 0, gz: 0, gt: 0, mx: 0, my: 0, mz: 0, mt: 0 };
   }
   
   componentDidMount() {
@@ -51,6 +56,11 @@ export default class App extends React.Component {
         <Value name="gy" value={this.state.gy} />
         <Value name="gz" value={this.state.gz} /> 
         <Value name="gt" value={this.state.gt} />
+        <Text style={styles.headline}>Magnetometer values</Text>
+        <Value name="mx" value={this.state.mx} />
+        <Value name="my" value={this.state.my} />
+        <Value name="mz" value={this.state.mz} />
+        <Value name="mt" value={this.state.mt} />
       </View>
       
     );
